f3fb257bd3b9cb6616338708d5f91eef5057bce6
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-04-11 10:06:49 +0300

    feat: shutdown params + refactoring

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"example/go_microservices_playground/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -25,5 +27,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func(){
+		err := s.ListenAndServe()
+
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	// graceful shutdown
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
